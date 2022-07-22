4852c9403755a3814d5542bc8c42148f8ad4a978
@ThViviani <developsviviani@gmail.com>
2022-07-22 21:32:20 +1000

    add layout for login page

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="app">
+  <div class="app" :class="{ loginColor: isSignIn }">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+export default {
+  computed: {
+    isSignIn() {
+      return this.$route.path === "/login";
+    },
+  },
+};
 </script>
 
 <style>
+.app {
+  height: 100vh;
+  width: 100vw;
+  background-color: #E2EEE3;
+}
+
+.loginColor {
+  background-color: #eaeaea;
+}
 </style>
--- vue-project/src/pages/Login.vue
+++ vue-project/src/pages/Login.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h1>Login page</h1>
+  <div class="login">
+    <form action="/my-handling-form-page" method="post">
+      <label>Вход в систему</label>
+      <input type="text" id="log" name="user_login" value="Логин" />
+      <input type="text" id="password" name="user_password" value="Пароль" />
+      <div class="save-container">
+        <input type="checkbox" id="save" name="selected_save" />
+        <label for="save" class="save-label">Запомнить</label>
+      </div>
+      <div class="submit-button">
+        <button type="submit">Далее</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -8,5 +19,72 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+.login {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+form {
+  background: rgba(255, 255, 255, 0.58);
+  border: 1px solid #47af52;
+  border-radius: 30px;
+  width: 31.04%;
+  height: 61.11%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  flex-shrink: 2;
+}
+
+label {
+  color: #00b268;
+  font-size: 2rem;
+  text-align: center;
+}
+
+button {
+  background: #47af52;
+  color: #ffffff;
+  font-size: 1.5rem;
+  border-radius: 30px;
+  border-width: 0;
+  min-height: 100%;
+  min-width: 100%;
+}
+
+.submit-button {
+    width: 39.76%;
+    height: 9.09%;
+}
+
+.save-container {
+  display: flex;
+  align-items: center;
+}
+
+input,
+.save-label {
+  font-size: 1.25rem;
+  color: #00000085;
+}
+
+input[type="checkbox"] {
+  min-height: 17px;
+  min-width: 17px;
+}
+
+input {
+  width: 80.8%;
+  background: rgba(255, 255, 255, 0);
+  border-bottom-color: #00b268;
+  border-top-style: none;
+  border-left-style: none;
+  border-right-style: none;
+}
 </style>
