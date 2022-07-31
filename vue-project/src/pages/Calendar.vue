d5ad14f2a6ea7ab1ba1251b3aadf834b5d642ae6
Bozhedom <c.cffc@mail.ru>
2022-07-31 09:33:12 +0900

    add burger-nav_bar

--- vue-project/src/pages/Calendar.vue
+++ vue-project/src/pages/Calendar.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <nav-bar></nav-bar>
+
     <h1>Calendar page</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: { NavBar},
+};
 </script>
 
 <style>
--- vue-project/src/pages/MeetingResults.vue
+++ vue-project/src/pages/MeetingResults.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <nav-bar></nav-bar>
+
     <h1>Meeting results page</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: { NavBar},
+};
 </script>
 
 <style>
