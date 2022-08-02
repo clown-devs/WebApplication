cae4db82277edbe24946c8a55768ff0a8ba96216
@ThViviani <developsviviani@gmail.com>
2022-08-02 23:11:12 +1000

    fixed: take away json data from Clients page

--- vue-project/src/pages/Clients.vue
+++ vue-project/src/pages/Clients.vue
@@ -2,33 +2,20 @@
   <div>
     <nav-bar></nav-bar>
     <h1>Clients page</h1>
-    <ul v-if="isLoadedPlaces">
-      <li v-for="place in places" :key="place">{{ place }}</li>
-    </ul>
-    <loading-indicate v-else></loading-indicate>
+    <loading-indicate></loading-indicate>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 import LoadingIndicate from "@/components/UI/LoadingIndicate.vue";
-import api from "@/api";
 
 export default {
   components: { NavBar, LoadingIndicate },
 
   data() {
-    return {
-      isLoadedPlaces: false,
-      places: [],
-    };
-  },
-
-  async mounted() {
-    const places = await api.getPlaces();
-    this.places = places;
-    this.isLoadedPlaces = true;
-  },
+    return {};
+  }
 };
 </script>
 
