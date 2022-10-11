ade1ff83ef782d52b0d0aba7f9c0768a6e93c717
@ThViviani <developsviviani@gmail.com>
2022-10-12 01:51:16 +1000

    test auto deploy

--- vue-project/deploy/deploy.js
+++ vue-project/deploy/deploy.js
@@ -4,10 +4,10 @@ const API_KEY = 'api/v2';
 const API_METHOD = '/deploy';
 
 console.log("deploy...");
-// axios.get(
-//     url + API_KEY + API_METHOD + '?password=123'
-// ).then(function (response) {
-//     console.log(response.data);
-// }).catch(function (error) {
-//     console.log(error.code);
-// });
+axios.get(
+    url + API_KEY + API_METHOD + '?password=123'
+).then(function (response) {
+    console.log(response.data);
+}).catch(function (error) {
+    console.log(error.code);
+});
