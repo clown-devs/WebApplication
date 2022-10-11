4ce4bbf3bb98b84919aea649b9c7c1fc6b681ff5
@ThViviani <developsviviani@gmail.com>
2022-10-12 00:09:58 +1000

    add deploy bash script

---
+++ vue-project/deploy/deploy.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const url = 'http://sbermeeting.tk/';
+const API_KEY = 'api/v2';
+const API_METHOD = '/deploy';
+
+console.log("deploy...");
+// axios.get(
+//     url + API_KEY + API_METHOD + '?password=123'
+// ).then(function (response) {
+//     console.log(response.data);
+// }).catch(function (error) {
+//     console.log(error.code);
+// });
