2cc97bf96edc9f0fb484830ea164e8867b369db2
@ThViviani <developsviviani@gmail.com>
2022-11-09 23:24:49 +1000

    add structure of calendar's components and logic for switching between calendar slice buttons

---
+++ vue-project/src/components/Calendar/CalendarContainer.vue
@@ -0,0 +1,61 @@
+<template>
+  <calendar-day-slice v-show="isPressedDayBtn"></calendar-day-slice>
+  <calendar-week-slice v-show="isPressedWeekBtn"></calendar-week-slice>
+  <calendar-month-slice v-show="isPressedMonthBtn"></calendar-month-slice>
+</template>
+
+<script>
+import CalendarDaySlice from "@/components/Calendar/CalendarDaySlice";
+import CalendarWeekSlice from "@/components/Calendar/CalendarWeekSlice";
+import CalendarMonthSlice from "@/components/Calendar/CalendarMonthSlice";
+
+export default {
+  name: "CalendarContainer",
+
+  components: {
+    CalendarDaySlice,
+    CalendarWeekSlice,
+    CalendarMonthSlice
+  },
+
+  data() {
+    return {
+      isPressedDayBtn: true,
+      isPressedWeekBtn: false,
+      isPressedMonthBtn: false
+    }
+  },
+
+  methods: {
+    activateSliceBtn(isPressedButtonsState = {day: false, week: false, month: false}) {
+      this.isPressedMonthBtn = isPressedButtonsState.month;
+      this.isPressedWeekBtn = isPressedButtonsState.week;
+      this.isPressedDayBtn = isPressedButtonsState.day;
+    }
+  },
+
+  watch: {
+    isPressedDayBtn(newState) {
+      if (newState) {
+        this.activateSliceBtn({day: true, month: false, week: false});
+      }
+    },
+
+    isPressedWeekBtn(newState) {
+      if (newState) {
+        this.activateSliceBtn({day: false, week: true, month: false});
+      }
+    },
+
+    isPressedMonthBtn(newState) {
+      if (newState) {
+        this.activateSliceBtn({day: false, week: false, month: true});
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/Calendar/CalendarDaySlice.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="day-container">
+    <h1>Day-container</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalendarDaySlice"
+}
+</script>
+
+<style scoped>
+
+</style>
