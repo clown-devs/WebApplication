71d58ee65c3e56bfe13fb14e05272098f87a0382
Даниил Божедомов <c.cffc@mail.ru>
2022-11-15 01:10:23 +1000

    add beta-version small calendar

--- vue-project/src/components/Calendar/SmallCalendar/SmallCalendar.vue
+++ vue-project/src/components/Calendar/SmallCalendar/SmallCalendar.vue
@@ -1,60 +1,88 @@
 <template>
   <div class="small-calendar-container">
     <header class="header-small-calendar">
-      <h2 class="header-date">Октябрь 2022</h2>
-      <i class="material-icons icon left"> arrow_left </i>
-      <i class="material-icons icon right"> arrow_right </i>
+      <h3 class="header-date"> {{ this.month[currentMonth] }} {{ currentYear }} </h3>
+      <i @click='changeMonth' class="material-icons icon left" id="prev"> arrow_left </i>
+      <i @click='changeMonth' class="material-icons icon right" id="next"> arrow_right </i>
     </header>
     <hr>
 
     <main class="main-grid">
       <ul class="calendar">
-        <li class="day day-name">П</li>
-        <li class="day day-name">В</li>
-        <li class="day day-name">С</li>
-        <li class="day day-name">Ч</li>
-        <li class="day day-name">П</li>
-        <li class="day day-name">С</li>
-        <li class="day day-name">В</li>
+        <li class="day-name">П</li>
+        <li class="day-name">В</li>
+        <li class="day-name">С</li>
+        <li class="day-name">Ч</li>
+        <li class="day-name">П</li>
+        <li class="day-name">С</li>
+        <li class="day-name">В</li>
+        <li v-for="prevDay in prevDays" :key="prevDay" class="day inactive"> {{ this.prevDays[prevDay - 1] }} </li>
 
-        <li class="day">1</li>
-        <li class="day">2</li>
-        <li class="day">3</li>
-        <li class="day">4</li>
-        <li class="day">5</li>
-        <li class="day">6</li>
-        <li class="day">7</li>
-        <li class="day">8</li>
-        <li class="day">9</li>
-        <li class="day">10</li>
-        <li class="day">11</li>
-        <li class="day">12</li>
-        <li class="day">13</li>
-        <li class="day">14</li>
-        <li class="day">15</li>
-        <li class="day">16</li>
-        <li class="day">17</li>
-        <li class="day">18</li>
-        <li class="day">19</li>
-        <li class="day">20</li>
-        <li class="day">21</li>
-        <li class="day">22</li>
-        <li class="day">23</li>
-        <li class="day">24</li>
-        <li class="day">25</li>
-        <li class="day">26</li>
-        <li class="day">27</li>
-        <li class="day">28</li>
-        <li class="day">29</li>
-        <li class="day">30</li>
-        <li class="day">31</li>
+        <li v-for="day in days" :key="day" class="day"> {{ this.days[day - 1] }} </li>
       </ul>
     </main>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+  data() {
+    return {
+      date: "",
+      currentYear: "",
+      currentMonth: "",
+      month: [
+        "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь",
+      ],
+      days: [],
+      prevDays: [],
+      lastDateOfMonth: "",
+      firstDayOfMonth: "",
+      lastDateOfLastMonth: "",
+      numberOfDay: "",
+    };
+  },
+
+  mounted() {
+    // assign a Date
+    this.date = new Date();
+    this.currentYear = this.date.getFullYear();
+    this.currentMonth = this.date.getMonth();
+
+    this.ConsoleLog();
+    this.renderCalendar();
+  },
+
+  methods: {
+    ConsoleLog() {
+    },
+
+    renderCalendar() {
+      this.firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay();
+      this.lastDateOfMonth = new Date(this.currentYear, Number(this.currentMonth) + 1, 0).getDate();
+      this.lastDateOfLastMonth = new Date(this.currentYear, this.currentMonth, 0).getDate();
+
+      for (let i = this.firstDayOfMonth; i > 1; i--) {
+        this.prevDays.push(this.lastDateOfLastMonth - i + 1);
+      }
+
+      console.log(this.prevDays )
+
+      for (let i = 1; i <= this.lastDateOfMonth; ++i) {
+        this.days.push(i);
+      }
+    },
+
+    changeMonth(icon) {
+      this.currentMonth = icon.target.id === "prev" ? this.currentMonth - 1 : this.currentMonth + 1;
+      this.prevDays = [];
+      this.days = [];
+      this.renderCalendar();
+    }
+
+  }
+}
 </script>
 
 <style scoped>
@@ -77,16 +105,16 @@ export default {}
 }
 
 .header-date {
-  margin: 0 10px 0 0;
+  margin: 0 auto 0 40px;
   padding: 0;
 }
 
 .icon {
-  height: 33px;
-  width: 33px;
+  height: 39px;
+  width: 39px;
   text-align: center;
-  line-height: 33px;
-  font-size: 33px;
+  line-height: 39px;
+  font-size: 39px;
   cursor: pointer;
   margin: 0 1px;
   border-radius: 50%;
@@ -95,6 +123,10 @@ export default {}
 
 .left {
   margin-right: 5px;
+}
+
+.right {
+  margin-right: 40px;
 }
 
 hr {
@@ -108,6 +140,7 @@ hr {
 
 .main-grid {
   width: 260px;
+  padding-right: 40px;
 }
 
 .calendar {
@@ -117,14 +150,32 @@ hr {
   margin: 0 0 25px 0;
 }
 
+.calendar li {
+  position: relative;
+}
+
+
 .day {
   list-style: none;
   text-align: center;
+  cursor: pointer;
+  z-index: 1;
 }
 
+.day.inactive {
+  color: #aaa;
+}
+
+.day.active {
+  color: #fff;
+}
+
+
 .calendar .day-name {
-  font-width: 700px;
+  font-weight: 700;
   font-size: 14px;
+  list-style: none;
+  text-align: center;
 }
 
 .grid {
@@ -150,5 +201,29 @@ hr {
 /* Animation main */
 .icon:hover {
   background: #f2f2f2;
+}
+
+.day::before {
+  position: absolute;
+  content: "";
+  height: 23px;
+  width: 23px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  z-index: -1;
+}
+
+.day:hover::before {
+  background: #f2f2f2;
+}
+
+.day.active::before {
+  background: #00B268;
+}
+
+.icon:active {
+  color: #00B268;
 }
 </style>
